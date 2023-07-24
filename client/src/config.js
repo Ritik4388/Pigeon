07322c9329cdf3d40710f8733b0f10b81f2d52e6
Ritik <ritikbharti4388@gmail.com>
2023-07-24 13:49:48 +0530

    update

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://pigeon-production.up.railway.app/";
+let BASE_URL = "https://pigeon-backend-dbid.onrender.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:4000/";
 }
